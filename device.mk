a27797fdac428c45d1193148a5f503e1f3088d44
ahmedhridoy371 <ahmedhridoy371@gmail.com>
2022-11-10 22:49:07 +0600

    lavender : Overlay:Moved Pe To overlay-extra
    Signed-off-by: ahmedhridoy371 <ahmedhridoy371@gmail.com>

--- device.mk
+++ device.mk
@@ -17,7 +17,7 @@ PRODUCT_SHIPPING_API_LEVEL := 28
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pe
+    $(LOCAL_PATH)/overlay-extra
 
 # Display Device Config
 PRODUCT_COPY_FILES += \
