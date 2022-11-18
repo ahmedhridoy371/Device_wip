ed4bfecf57c47065be03e6fa87247259ffbd9bb7
ahmedhridoy371 <ahmedhridoy371@gmail.com>
2022-11-18 20:36:51 +0600

    lavender: corvusOs Bringup
    Signed-off-by: ahmedhridoy371 <ahmedhridoy371@gmail.com>

--- corvus_lavender.mk
+++ corvus_lavender.mk
@@ -7,19 +7,28 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common PixelExperience stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-TARGET_BOOT_ANIMATION_RES := 1080
+# Inherit some common CorvusOS stuff
+$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# CorvusOS
+RAVEN_LAIR=Unofficial
+CORVUS_MAINTAINER=Apex_Not_Legend
+USE_GAPPS=true
+USE_CUSTOM_CLANG=true
+TARGET_INCLUDE_PIXEL_CHARGER := true
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 $(call inherit-product, $(LOCAL_PATH)/device-hidl.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_lavender
+PRODUCT_NAME := corvus_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
---
+++ product_config.mk
@@ -0,0 +1,4 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/corvus_lavender.mk
+
+COMMON_LUNCH_CHOICES := corvus_lavender-userdebug
