74efe0eecef4dbb36eabe06083d8fa8431c1a012
ahmedhridoy371 <ahmedhridoy371@gmail.com>
2022-11-18 20:11:09 +0600

    lavender: riceDroid Bringup
    Signed-off-by: ahmedhridoy371 <ahmedhridoy371@gmail.com>

--- lineage_lavender.mk
+++ lineage_lavender.mk
@@ -8,11 +8,48 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff
+# Inherit some common riceDroid stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-TARGET_BOOT_ANIMATION_RES := 1080
+# maintainer flag (one word only/no spacing)
+RICE_MAINTAINER := Apex_Not_Legend
+
+# chipset flag (one word only/no spacing)
+RICE_CHIPSET := CpuDead
+
+# gapps build flag, if not defined build type is vanilla
+WITH_GMS := true
+
+# Sushi Bootanimation (only 720/1080p/1440 supported. if not defined, bootanimation is google bootanimation)
+SUSHI_BOOTANIMATION := 720
+
+# Graphene Camera
+TARGET_BUILD_GRAPHENEOS_CAMERA := true
+
+# disable/enable blur support, default is false
+TARGET_ENABLE_BLUR := true
+
+# UDFPS ICONS/ANIMATIONS
+TARGET_HAS_UDFPS := false
+
+# Quick tap feature
 TARGET_SUPPORTS_QUICK_TAP := true
+
+# Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Memory properties
+# Tune these properties according to device perfconfigstore.xml(if available)/device capabilities.
+# These props are overridable
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.fw.bg_apps_limit?=48 \
+    persist.sys.fw.use_trim_settings?=true \
+    persist.sys.fw.empty_app_percent?=50 \
+    persist.sys.fw.trim_empty_percent?=100 \
+    persist.sys.fw.trim_cache_percent?=100 \
+    persist.sys.fw.trim_enable_memory?=2147483648 \
+    persist.sys.fw.bservice_age?=120000 \
+    persist.sys.fw.bservice_limit?=6 \
+    persist.sys.fw.bservice_enable?=true
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
