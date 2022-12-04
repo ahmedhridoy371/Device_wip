728293c9a7b172c7afec8ad07cef8620e71312de
ahmedhridoy371 <ahmedhridoy371@gmail.com>
2022-12-04 23:32:19 +0600

    lavender: ArrowOs Bringup
    Signed-off-by: ahmedhridoy371 <ahmedhridoy371@gmail.com>

--- arrow_lavender.mk
+++ arrow_lavender.mk
@@ -8,18 +8,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common PixelExperience stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-TARGET_BOOT_ANIMATION_RES := 1080
+# Inherit common ArrowOS configurations
+$(call inherit-product, vendor/arrow/config/common.mk)
+TARGET_BOOT_ANIMATION_RES := 720
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
+DEVICE_MAINTAINER := Apex_Not_Legend
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 $(call inherit-product, $(LOCAL_PATH)/device-hidl.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_lavender
+PRODUCT_NAME := arrow_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
---
+++ product_config.mk
@@ -0,0 +1,4 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/arrow_lavender.mk
+
+COMMON_LUNCH_CHOICES := arrow_lavender-userdebug
