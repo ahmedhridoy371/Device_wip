4f82c9b171b750ac57691b0087b64eec960c68f8
ahmedhridoy371 <ahmedhridoy371@gmail.com>
2022-12-16 13:19:50 +0600

    lavender: calyxOs Bringup
    Signed-off-by: ahmedhridoy371 <ahmedhridoy371@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_lavender.mk
+    $(LOCAL_DIR)/calyx_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_lavender-user \
-    aosp_lavender-userdebug \
-    aosp_lavender-eng
+    calyx_lavender-user \
+    calyx_lavender-userdebug \
+    calyx_lavender-eng
---
+++ product_config.mk
@@ -0,0 +1,4 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/calyx_lavender.mk
+
+COMMON_LUNCH_CHOICES := calyx_lavender-userdebug
