8fe40c14c752f18e7e4147d6e94362fa21a6f6b5
ahmedhridoy371 <ahmedhridoy371@gmail.com>
2022-12-18 22:18:52 +0600

    lavender: stagOS Bringup
    Signed-off-by: ahmedhridoy371 <ahmedhridoy371@gmail.com>

---
+++ product_config.mk
@@ -0,0 +1,4 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/stag_lavender.mk
+
+COMMON_LUNCH_CHOICES := stag_lavender-userdebug
--- stag_lavender.mk
+++ stag_lavender.mk
@@ -8,9 +8,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common PixelExperience stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-TARGET_BOOT_ANIMATION_RES := 1080
+# Inherit some common stag stuff
+$(call inherit-product, vendor/stag/main.mk)
+TARGET_BOOT_ANIMATION_RES := 720
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
@@ -19,7 +19,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 $(call inherit-product, $(LOCAL_PATH)/device-hidl.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_lavender
+PRODUCT_NAME := stag_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
