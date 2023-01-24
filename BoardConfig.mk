296531b39893db24e049e4ad4564b9c63d8eda46
wHo-EM-i <ehteshammalik18998@gmail.com>
2023-01-24 07:11:17 +0530

    lavender: Use CAF configuration
    Signed-off-by: Prashant-1695 <prashant33968@gmail.com>
    Signed-off-by: ahmedhridoy371 <ahmedhridoy371@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,7 +13,7 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a73
+TARGET_CPU_VARIANT := generic
 
 ifeq (,$(filter %_64,$(TARGET_PRODUCT)))
 TARGET_2ND_ARCH := arm
