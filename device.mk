494ef0b9c690bf5ef4b1d95c86ccffebb030a405
Michael Bestas <mkbestas@lineageos.org>
2023-03-25 04:59:25 +0000

    lavender:  Move qti_whitelist.xml to /system_ext
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>
    Signed-off-by: ahmedhridoy371 <ahmedhridoy371@gmail.com>

--- device.mk
+++ device.mk
@@ -361,7 +361,7 @@ PRODUCT_COPY_FILES += \
 # QCOM
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/qti_whitelist.xml
 
 # QMI
 PRODUCT_PACKAGES += \
